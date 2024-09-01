17db4e7e07dfc30bd5ac328a3d7d7aabf0cb46de
Chandra shekhara prasad <chandrashekharaprasad@gmail.com>
2024-09-02 02:57:16 +0530

    adding Question no 3

---
+++ Number.java
@@ -0,0 +1,14 @@
+// . Write a java program to find greatest among three numbers?
+public class Number {
+    public static void main(String[] args) {
+
+        int number1 = Integer.parseInt(args[0]);
+        int number2 = Integer.parseInt(args[1]);
+        int number3= Integer.parseInt(args[3]);
+     // int g= (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        int greatest = (number1 > number2)? (number1 > number3)? number1 : number3 : (number2 > number3)? number2  : number3 ;
+        System.out.println("the Greatest Number thilu : "+greatest);
+        
+    }
+    
+}
